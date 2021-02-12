6190dc9402820cf26116c0c17d021030a0486d68
DylanNg-coder <p3rs0nal1tY>
2021-02-12 09:55:43 +0000

    finally got it to say hello world

--- src/main/java/com/example/omanda/SpringBootTutorialApplication.java
+++ src/main/java/com/example/omanda/SpringBootTutorialApplication.java
@@ -2,10 +2,12 @@ package com.example.omanda;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
+@ComponentScan("com.example.omanda")
 public class SpringBootTutorialApplication {
 
 
@@ -13,9 +15,6 @@ public class SpringBootTutorialApplication {
 		SpringApplication.run(SpringBootTutorialApplication.class, args);
 	}
 
-	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
-	}
+
 
 }
